7862b4b86e45758ec2e6c4b2a17a8319a361f9ce
yurianisimov27 <yurianisimov27@gmail.com>
2020-07-02 00:47:01 +0300

    19. route & browser-router

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = (props) => {
   return (
-    <div className='app_wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app_wrapper_content'>
-          <Dialogs />
-      </div>
-        {/*<Profile />*/}
-    </div>
+      <BrowserRouter>
+          <div className='app_wrapper'>
+              <Header />
+              <Navbar />
+              <div className='app_wrapper_content'>
+                  <Route path='/dialogs' component={Dialogs} />
+                  <Route path='/profile' component={Profile} />
+                  {/*<Dialogs />*/}
+              </div>
+              {/*<Profile />*/}
+          </div>
+      </BrowserRouter>
   );
 }
 
