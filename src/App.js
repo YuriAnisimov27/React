27903b0d76205432b023a80de40278a39d67dc36
yurianisimov27 <yurianisimov27@gmail.com>
2020-07-07 20:03:01 +0300

    29. added state.js with data

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ const App = (props) => {
               <Header />
               <Navbar />
               <div className='app_wrapper_content'>
-                  <Route path='/dialogs' render={ () => <Dialogs messages={props.messages} dialogs={props.dialogs}/> } />
-                  <Route path='/profile' render={ () => <Profile posts={props.posts}/> } />
-
+                  <Route path='/dialogs'
+                         render={ () => <Dialogs state={props.state.dialogsPage} /> } />
+                  <Route path='/profile'
+                         render={ () => <Profile state={props.state.profilePage} /> } />
                   <Route path='/music' component={Music} />
                   <Route path='/news' component={News} />
                   <Route path='/settings' component={Settings} />
---
+++ src/redux/state.js
@@ -0,0 +1,30 @@
+
+let state = {
+    profilePage: {
+        posts: [
+            { id: 1, post: "it's my first post", like_count: 37 },
+            { id: 2, post: 'Hello World!', like_count: 12 },
+            { id: 3, post: 'Hi, how are you?', like_count: 40 },
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            { id: 1, name: 'Dima' },
+            { id: 2, name: 'Vova' },
+            { id: 3, name: 'Masha' },
+            { id: 4, name: 'Igor' },
+            { id: 5, name: 'Klava' },
+            { id: 6, name: 'Jack' },
+            { id: 7, name: 'Jhon' }
+        ],
+        messages: [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'Hello World!' },
+            { id: 3, message: 'BinPot)' },
+            { id: 4, message: 'How is your name?' },
+            { id: 5, message: 'How a u?' },
+        ]
+    }
+}
+
+export default state;
