accd756a1545c12dd5fbd3fc35b8688643c1228c
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-08-13 23:08:46 +0300

    feat: add loader

---
+++ src/components/common/Preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "../../../assets/images/loading.svg";
+import React from "react";
+
+const Preloader = (props) => {
+    return <img src={preloader}/>
+}
+
+export default Preloader;
